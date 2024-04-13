09a533f932c8e882ece617e6e07c841de5eb6907
f4xt <f4xt@earth.com>
2024-04-13 16:50:59 +0300

    Install required vscode extensions from init.sh

--- init.sh
+++ init.sh
@@ -3,3 +3,5 @@ cp .gdbinit ~/
 cp .screenrc ~/
 cp settings.json ~/.config/Code/User/
 cp keybindings.json ~/.config/Code/User/
+code --install-extension yeannylam.insert-new-line
+code --install-extension ericmccarthy.vs-code-mark-point
