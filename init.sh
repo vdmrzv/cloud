e606281c9c4379b98172222a9b434c9968f71505
a-whole-new-world <32310859+a-whole-new-world@users.noreply.github.com>
2024-09-18 17:32:54 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,4 @@
-cp .nanorc ~/
-cp .gdbinit ~/
-cp .screenrc ~/
-cp settings.json ~/.config/Code/User/
-cp keybindings.json ~/.config/Code/User/
+cp .nanorc .screenrc .gdbinit .gdbearlyinit stl-views.gdb redirect-output.gdb ~/
+cp settings.json keybindings.json ~/.config/Code/User/
 code --install-extension yeannylam.insert-new-line
 code --install-extension ericmccarthy.vs-code-mark-point
