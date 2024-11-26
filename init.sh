fd1f05fc2a5bc565c7acbb8f91edad1f98901ca4
Vadim Morozov <v.morozov@smartengines.com>
2024-11-26 16:39:46 +0300

    download and install better nano syntax highlight

--- init.sh
+++ init.sh
@@ -2,3 +2,4 @@ cp .nanorc .screenrc .gdbinit .gdbearlyinit stl-views.gdb redirect-output.gdb ~/
 cp settings.json keybindings.json ~/.config/Code/User/
 code --install-extension yeannylam.insert-new-line
 code --install-extension ericmccarthy.vs-code-mark-point
+wget https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh -O- | sh
