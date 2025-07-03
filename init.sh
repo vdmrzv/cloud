006bf9e6f82a98259cb7994d7e6eda95ef2b7527
vdmrzv <vdmrzv@proton.me>
2025-07-03 12:57:57 +0300

    do not forget to copy .bashrc also

--- init.sh
+++ init.sh
@@ -1,6 +1,11 @@
+apt install git nano screen tree
+cp .bashrc .nanorc .screenrc .gdbinit .gdbearlyinit stl-views.gdb redirect-output.gdb ~/
+
+# nano syntax
+wget https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh -O- | sh
+
+# vscode
 mkdir ~/.config/Code/User/
-cp .nanorc .screenrc .gdbinit .gdbearlyinit stl-views.gdb redirect-output.gdb ~/
 cp settings.json keybindings.json ~/.config/Code/User/
 code --install-extension yeannylam.insert-new-line
 code --install-extension ericmccarthy.vs-code-mark-point
-wget https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh -O- | sh
