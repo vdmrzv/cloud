e6dab4f57ea8db7a55621a6cc63677cbe9ba3c40
vdmrzv <vdmrzv@proton.me>
2025-07-14 08:24:45 +0300

    add -p option to mkdir call

--- init.sh
+++ init.sh
@@ -1,11 +1,11 @@
-apt install git nano screen tree
-cp .bashrc .nanorc .screenrc .gdbinit .gdbearlyinit stl-views.gdb redirect-output.gdb ~/
+apt install nano tree screen
+cp .bashrc .nanorc .screenrc .gdbinit .gdbearlyinit ~/
 
 # nano syntax
 wget https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh -O- | sh
 
 # vscode
-mkdir ~/.config/Code/User/
+mkdir -p ~/.config/Code/User/
 cp settings.json keybindings.json ~/.config/Code/User/
 code --install-extension yeannylam.insert-new-line
 code --install-extension ericmccarthy.vs-code-mark-point
